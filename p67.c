56217c07cfb4c6a340657afd148da9f64ae932fc
kavi0310 <46838903+kavi0310@users.noreply.github.com>
2019-03-15 00:12:12 +0530

    Update p67.c

--- p67.c
+++ p67.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
 int main() {
-   int n,fact=1,i;
+   int n,f=1,i;
    scanf("%d",&n);
    for(i=1;i<=n;i++)
    {
-       fact=fact*i;
+       f=f*i;
    }
-   printf("%d",fact);
+   printf("%d",f);
   return 0;
 }
